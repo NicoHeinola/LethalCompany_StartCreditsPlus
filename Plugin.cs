f671d88d90bf3e28e0b32ef0c50acbd25ab258e8
Nico Heinola <nico.heinola@gmail.com>
2025-01-11 18:42:47 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace StartCreditsPlus;
+
+[BepInPlugin(modGUID, modName, modVersion)]
+public class StartCreditsPlusPlugin : BaseUnityPlugin
+{
+    private const string modGUID = "Pizza.StartCreditsPlus";
+    private const string modName = "Start Credits Plus";
+    private const string modVersion = "1.0.0";
+
+    private readonly Harmony harmony = new Harmony(modGUID);
+
+    private static StartCreditsPlusPlugin instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        harmony.PatchAll(typeof(StartCreditsPlusPlugin));
+    }
+}
